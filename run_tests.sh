7cb39ecc1672f07c3fdef10e7864a7131619c864
Pandawan <pandawanfr@gmail.com>
2020-05-29 18:36:51 -0700

    Better test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-# Debug Mode: ./run_test.sh
-# Release Mode: ./run_test.sh release
+# Local Debug Mode: ./run_tests.sh
+# Add "release" to build a release version of the rust lib
+# Or add "online" to run the tests using the online lib instead
+# Example: ./run_tests.sh online
 
 set -e
 
-if [[ $1 = "release" ]]; then
+# Build in release mode if needed
+if [[ $1 = "release" ]];  then
     cargo build -p deno_notify --release
-else
+
+    DENO_NOTIFY_PLUGIN_BASE=./target/release/ 
+
+# Build in debug mode if not release and not online
+elif [[ $1 != "online" ]];  then
     cargo build -p deno_notify
+
+    DENO_NOTIFY_PLUGIN_BASE=./target/debug/
 fi
 
-DENO_NOTIFY_PLUGIN_BASE=./target/debug/ DENO_NOTIFY_DEBUG=true \
+DENO_NOTIFY_DEBUG=true
 deno test --unstable --allow-all tests/test.ts
