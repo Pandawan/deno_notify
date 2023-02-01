89ed931c17a68fcefc29349b750dabcd84e69efa
Pandawan <pandawanfr@gmail.com>
2023-02-01 13:46:52 -0800

    Update to support Deno 1.25

--- ts/plugin.ts
+++ ts/plugin.ts
@@ -1,22 +1,21 @@
-import { Plug } from "https://deno.land/x/plug@0.5.1/mod.ts";
+import { CacheSetting, dlopen } from "https://deno.land/x/plug@1.0.0/mod.ts";
 
-const VERSION = "1.3.1";
-const POLICY = Deno.env.get("NOTIFY_PLUGIN_URL") === undefined
-  ? Plug.CachePolicy.STORE
-  : Plug.CachePolicy.NONE;
+const VERSION = "1.4.0";
+const CACHE_POLICY: CacheSetting =
+  Deno.env.get("NOTIFY_PLUGIN_URL") === undefined ? "use" : "reloadAll";
 const NOTIFY_PLUGIN_URL = Deno.env.get("NOTIFY_PLUGIN_URL") ??
   `https://github.com/Pandawan/deno_notify/releases/download/${VERSION}/`;
 
-const library = await Plug.prepare({
+const library = await dlopen({
   name: "deno_notify",
   url: NOTIFY_PLUGIN_URL,
-  policy: POLICY,
+  cache: CACHE_POLICY,
 }, {
-  notify_send: { parameters: ["pointer", "usize"], result: "pointer" },
+  notify_send: { parameters: ["buffer", "usize"], result: "buffer" },
 });
 
-function readPointer(v: any): Uint8Array {
-  const ptr = new Deno.UnsafePointerView(v as Deno.UnsafePointer);
+function readPointer(v: Deno.PointerValue): Uint8Array {
+  const ptr = new Deno.UnsafePointerView(v);
   const lengthBe = new Uint8Array(4);
   const view = new DataView(lengthBe.buffer);
   ptr.copyInto(lengthBe, 0);
@@ -39,6 +38,7 @@ function notify_send(json: string): NotifySendResult {
     encodedJson,
     encodedJson.length,
   );
+  // TODO: Use Deno.UnsafePointerView#getCString
   const decodedJson = new TextDecoder().decode(readPointer(ptrResult));
   const result = JSON.parse(decodedJson);
   return result as NotifySendResult;
